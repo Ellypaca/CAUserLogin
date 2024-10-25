955e7e039f46f6f764e6c2335ddb52ce2e3331a1
katelynnbai <ellypaca1234@gmail.com>
2024-10-25 04:57:43 -0400

    Task 2.1 Commit

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user with the given username.
+     * @param name the name to set as.
+     */
+    void setCurrentUser(String name);
 }
